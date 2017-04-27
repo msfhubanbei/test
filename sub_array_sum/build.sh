8a8a87dd657ecac7e485f55473bc337622a9a10a
shaofang ma <mashaofang@qiniu.com>
2017-04-27 20:39:14 +0800

    add sub array

---
+++ sub_array_sum/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+gcc -o find_sub_array.o -c find_sub_array.c 
+gcc -o find_sub_array find_sub_array.o
+chmod 777 find_sub_array
+
---
+++ sub_array_sum/find_sub_array.c
@@ -0,0 +1,55 @@
+// i :子数组长度    
+ 
+ #include <stdio.h>
+
+
+ 
+ int find_sub_len(int *array, int n,int key)
+ {
+    int i;//i :子数组长度 
+    int m; //m:首元素 平移几次
+    int j;//j:子数组首元素～子数组倒数第二元素
+    int k;//k:子数组末元素
+    int sum=0;
+    int flag;
+
+    for(int i=2;i<=9; i++)
+    for(m=0;m<n-i;m++)
+    for(j=m,k=j+i-1; k<n;k++)
+    {
+        
+        flag=i-1;
+        printf("i=%d,m=%d,j=%d, k=%d ,flag=%d\r\n",i,m,j,k,flag);
+        while(flag>0)//flag:  控制子数组前n－1个元素求和
+        {
+            sum+=array[j];
+            j++;
+            flag--;
+            printf("sum:%d \r\n",sum);
+        }
+        if(key==(sum+array[k]))
+        {
+            
+            printf("test:%d, %d\r\n",i,key);
+            return i;
+        }
+        else{
+            sum=0;
+            j=m;
+        }
+    }
+    return 0;
+
+ }
+ int main()
+ {
+    int array[]={1,5,2,4,6,3,1,19,50};
+    
+    int key=58;
+    int tag;
+    tag=find_sub_len(array, 9, key);
+    printf("sub array len: %d \r\n",tag);
+    return 0;
+}
+
+
